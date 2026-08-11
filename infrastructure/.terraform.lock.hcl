8c0e03a4e622d2d70e410a83b9f00e56e3e66ec0
Marco <arboleda.marco@gmail.com>
2026-08-11 17:52:56 -0400

    Add Airflow option for GCP resource setup

--- infrastructure/.terraform.lock.hcl
+++ infrastructure/.terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/google" {
   version     = "7.43.0"
   constraints = "7.43.0"
   hashes = [
+    "h1:2EZO7bntLiyBOYddCgpZrV9sGA5UPcMWUC6jFFw8UOc=",
     "h1:AwFYLjEh05Jog9DYlEQDacVk2Sjt5/QrKAsymAW0k/Q=",
     "zh:0050abe670a076cfc110662ba3e9f8c25366a63df9389dfbe090b5dc5e73a4d2",
     "zh:31769c27700334d11e652ddb2a0dfc9efd65df43dbc51a06afc04e10db645bbe",
---
+++ orchestration/dags/01_gcp_setup.py
@@ -0,0 +1,31 @@
+from airflow.sdk import dag
+from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+
+## Set these variables before executing the DAG
+PROJECT_ID = "your-gcp-project-id"
+BUCKET_NAME = "your-unique-gcs-bucket-name"  ## Recommendation: follow the pattern "<gcp-project-id>-<desired-bucket-name>" to ensure uniqueness.
+DATASET_NAME = "your_bigquery_dataset_name"
+REGION = "us-central1" # change if desired
+
+@dag
+def gcp_setup():
+    create_bucket = GCSCreateBucketOperator(
+        task_id="create_gcs_bucket",
+        bucket_name=BUCKET_NAME,
+        project_id=PROJECT_ID,
+        location=REGION,
+        storage_class="STANDARD",
+        gcp_conn_id="open_food_facts_gcp"  # Ensure this connection is set up in your Airflow with the correct credentials
+    )
+
+    create_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id="create_bigquery_dataset",
+        dataset_id=DATASET_NAME,
+        project_id=PROJECT_ID,
+        location=REGION,
+        gcp_conn_id="open_food_facts_gcp",
+        if_exists="log"
+    )
+
+gcp_setup()
