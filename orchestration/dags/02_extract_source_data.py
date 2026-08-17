1907b1a1bf4608cf15dd77a2001d358647a18170
Marco <arboleda.marco@gmail.com>
2026-08-17 17:50:07 -0400

    refactor: less duplicate code in 02_extract_source_data.py

--- orchestration/dags/02_extract_source_data.py
+++ orchestration/dags/02_extract_source_data.py
@@ -1,4 +1,5 @@
 from datetime import date
+from enum import Enum
 
 from airflow.sdk import dag
 from airflow.providers.google.cloud.operators.cloud_storage_transfer_service import \
@@ -8,23 +9,26 @@ GCP_PROJECT_ID = "your-gcp-project-id"
 GCS_BUCKET_NAME = "your-gcs-bucket-name"
 GCP_CONN_ID = "open_food_facts_gcp"  # Ensure this connection is set up in your Airflow with the correct credentials
 
-@dag
-def extract_source_data():
+class Source(Enum):
+    PARQUET = "parquet"
+    JSONL = "jsonl"
+
+def schedule_extraction_from_source(source: Source):
     date_today = date.today()
 
-    extract_parquet = CloudDataTransferServiceCreateJobOperator(
-        task_id="extract_parquet",
+    return CloudDataTransferServiceCreateJobOperator(
+        task_id=f"extract_{source.value}",
         body={
-            "description": "Extract Open Food Facts parquet data into GCS",
+            "description": f"Extract Open Food Facts {source.value} data into GCS",
             "status": GcpTransferJobsStatus.ENABLED,
             "projectId": GCP_PROJECT_ID,
             "transferSpec": {
                 "httpDataSource": {
-                    "listUrl": f"gs://{GCS_BUCKET_NAME}/source_parquet_url.tsv"
+                    "listUrl": f"gs://{GCS_BUCKET_NAME}/source_{source.value}_url.tsv"
                 },
                 "gcsDataSink": {
                     "bucketName": GCS_BUCKET_NAME,
-                    "path": f"open_food_facts_subset_{date_today}.parquet/"
+                    "path": f"open_food_facts_subset_{date_today}_{source.value}/"
                 }
             },
             "schedule": {
@@ -43,35 +47,11 @@ def extract_source_data():
         gcp_conn_id=GCP_CONN_ID
     )
 
-    extract_jsonl = CloudDataTransferServiceCreateJobOperator(
-        task_id="extract_jsonl",
-        body={
-            "description": "Extract Open Food Facts JSONL data into GCS",
-            "status": GcpTransferJobsStatus.ENABLED,
-            "projectId": GCP_PROJECT_ID,
-            "transferSpec": {
-                "httpDataSource": {
-                    "listUrl": f"gs://{GCS_BUCKET_NAME}/source_jsonl_url.tsv"
-                },
-                "gcsDataSink": {
-                    "bucketName": GCS_BUCKET_NAME,
-                    "path": f"open_food_facts_complete_{date_today}.jsonl.gz/"
-                }
-            },
-            "schedule": {
-                "scheduleStartDate": {
-                    "year": date_today.year,
-                    "month": date_today.month,
-                    "day": date_today.day
-                },
-                "startTimeOfDay": { # Set in UTC
-                    "hours": 23, # 7pm in ET
-                    "minutes": 0,
-                    "seconds": 0
-                }
-            }
-        },
-        gcp_conn_id=GCP_CONN_ID
-    )
+@dag
+def extract_source_data():
+
+    extract_parquet = schedule_extraction_from_source(Source.PARQUET)
+    
+    extract_jsonl = schedule_extraction_from_source(Source.JSONL)
 
 extract_source_data()
